59c7dfbad706dbca8f8d309a7d0e99a973f7d47c
Swarsel <leon.schwarzaeugl@gmail.com>
2024-07-30 15:00:54 +0200

    feat: new fork for custom miryoku
    the old fork was corrupted to a high degree
    after merging in a broken version of miryoku

---
+++ keyboards/splitkb/kyria/keymaps/Swarsel/rules.mk
@@ -0,0 +1,12 @@
+OLED_ENABLE = no
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+CONVERT_TO=liatris         # Convert from Elite-C to Liatris
+VIA_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE = no
+GRAVE_ESCAPE_ENABLE = no
+ENCODER_MAP_ENABLE = no
+KEY_LOCK_ENABLE = no
